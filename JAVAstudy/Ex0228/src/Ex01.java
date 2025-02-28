612f86887849fce147ceaab6eb8c699b5fa7869d
Kimhyerim <ax1021@naver.com>
2025-02-28 11:06:43 +0900

    [JAVAstudy] JAVAstudy 폴더 생성 / 자바 기초 실습

---
+++ JAVAstudy/Ex0228/src/Ex01.java
@@ -0,0 +1,16 @@
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		// 코드의 시작은 main 부터
+		// 코드의 방향은 위에서 아래로 진행됨
+		
+		// 출력하기
+		System.out.println(3 + 7);
+		System.out.println();
+		
+		
+	}
+
+}
---
+++ JAVAstudy/Ex0228/src/Ex02변수.java
@@ -0,0 +1,32 @@
+
+public class Ex02변수 {
+
+	public static void main(String[] args) {
+		
+		// 정수 형태의 데이터를 담을 수 있는 변수를 선언하겠습니다.
+		// 만들어진 변수의 이름을 num이라고 하겠습니다.
+		int num;
+		
+		// num이라는 공간에 데이터 10을 담아주겠습니다.
+		num = 10;
+		
+		// 위의 2줄을 합쳐서 1줄로 만들어주세요.
+		int num1 = 10;
+		
+		System.out.println(num);
+		
+		// 30을 num이 아닌 새로운 변수에 담아주세요.
+		int number = 30;
+		
+		// number의 값을 100으로 바꿔주세요.
+		num = 100;
+		
+		// 문자열 변수 만들기
+		String name;
+		
+		// 문자열 변수에 값 넣기
+		name = "승환";
+
+	}
+
+}
