e39028494f8114b8cdedc1db9ed2ec8d5ff17f40
Kimhyerim <ax1021@naver.com>
2025-03-07 16:10:49 +0900

    [JAVAstudy] 배열 연습

--- JAVAstudy/Ex05배열/src/배열실습/Ex07배열실습_거리비교.java
+++ JAVAstudy/Ex05배열/src/배열실습/Ex07배열실습_거리비교.java
@@ -16,11 +16,11 @@ public class Ex07배열실습_거리비교 {
 		int a = 0; // 인덱스 1 담기
 		int b = 0; // 인덱스 2 담기
 		int c = 0; // 두 인덱스 사이 거리
-		int d = point[0]; // 두 인덱스 사이 최소 거리
+		int d = Integer.MAX_VALUE; // 두 인덱스 사이 최소 거리
 
 		for (int i = 0; i < point.length; i++) {
 			for (int j = 0; j < point.length; j++) {
-				if (point[i] - point[j] > 0) {
+				if (point[i] - point[j] > 0) { // 음수 거르기
 					System.out.println(i + "회차 i = " + point[i]);
 					System.out.println(j + "회차 j = " + point[j]);
 					System.out.println(j + "회차 a = " + a);
@@ -28,21 +28,22 @@ public class Ex07배열실습_거리비교 {
 					System.out.println(j + "회차 c = " + c);
 					System.out.println(point[i] - point[j]);
 					
-					c = point[i] - point[j];
-					if (d > c) {
-						a = i;
+					c = point[i] - point[j]; // 현재 두 데이터 사이의 거리 저장
+					if (d > c) { // 현재 거리와 이전 최소 거리 비교
+						d = c; // 새로운 최소 거리 저장
+						
+						a = i; // 새로운 최소 거리의 인덱스 저장
 						b = j;
-						d = c;
+						
 					}
 				}
 			}
 		}
 
-		result[0] = a;
+		result[0] = a; // 최종 최소 거리 인덱스 저장
 		result[1] = b;
 
 		System.out.println("result = " + Arrays.toString(result));
 
 	}
-
 }
